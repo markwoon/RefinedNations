a6d7eb4819a20303aec4537b08222916a1616222
Mark Woon <markwoon@gmail.com>
2020-05-14 06:02:11 -0700

    chore: bump version

--- Refined Nations.user.js
+++ Refined Nations.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Refined Nations
-// @version      3.0.0
+// @version      3.0.1
 // @description  UI tweaks for MaBi Web Nations
 // @match        http://www.mabiweb.com/modules.php?name=GM_Nations*
 // @author       Mark Woon
